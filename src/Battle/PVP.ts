67813839cdaf4148d5c30772bbed8353afb1e33d
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-07-04 12:10:12 -0300

    fix:  PVP class

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,16 +1,21 @@
-import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
-export default class PVP extends Battle {
-  constructor(
-    private firstCharacter: Character,
-    private secondCharacter: Character,
-  ) {
-    super(firstCharacter);
+class PVP extends Battle {
+  private _enemy: Fighter;
+
+  constructor(player: Fighter, enemy: Fighter) {
+    super(player);
+    this._enemy = enemy;
   }
 
-  override fight() {
-    console.log(`ola ${this.firstCharacter}`);
-    return 1;
+  fight(): number {
+    if (this.player.lifePoints > 0 && this._enemy.lifePoints > 0) {
+      this.player.attack(this._enemy);
+      this._enemy.attack(this.player);
+    }
+    return this.player.lifePoints === -1 ? -1 : 1;
   }
-} 
+}
+
+export default PVP; 
