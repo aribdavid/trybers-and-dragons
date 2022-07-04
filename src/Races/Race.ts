aebc596d94241e2027f96d2f0917c2f07a259e57
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-07-04 11:30:36 -0300

    feat: created Race class

---
+++ src/Races/Race.ts
@@ -0,0 +1,17 @@
+export default abstract class Race {
+  constructor(private _name: string, private _dexterity:number) {}
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+
+  get name(): string {
+    return this._name;
+  } 
+
+  get dexterity(): number {
+    return this._dexterity;
+  } 
+}
